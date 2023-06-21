84c8dec8502761897b7da540c596ca2d54fbc350
khalil <khalilahmed232@gmail.com>
2023-06-21 22:50:57 +0530

    added requests library

--- hello.py
+++ hello.py
@@ -1,9 +1,23 @@
+import requests
 
 # Python program to execute
 # main directly
 print ("Always executed")
  
+# Making a GET request
+r = requests.get('https://api.github.com/users/naveenkrnl')
+
+# check status code for response received
+# success code - 200
+print(r)
+
+# print content of request
+print(r.content)
+
 if __name__ == "__main__":
     print ("Executed when invoked directly")
+
 else:
     print ("Executed when imported")
+
+   
